82f0c43a51c6e00947fde8d4bfd7e8640d8b2253
Jonahtan Saudhof <jonathan@jonathansaudhof.de>
2020-02-16 19:10:39 +0100

    first running setup of react app

--- frontendtest/src/App.js
+++ frontendtest/src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-class App extends React.Component {}
+import { home, login, dashboard } from './pages';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Router>
+					<Switch>
+						<Route exact path='/' component={home} />
+						<Route path='/user/dashboard' component={dashboard} />
+						<Route exact path='/login' componen={login} />
+					</Switch>
+				</Router>
+				<h1>Hello Test</h1>
+			</div>
+		);
+	}
+}
 
 export default App;
